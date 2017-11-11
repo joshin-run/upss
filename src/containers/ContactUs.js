b00d8287182ecb95e6459dceb005b30e44cc162b
joshin-run <jahmsc@gmail.com>
2017-11-10 21:17:52 -0700

    Feat (style) contact us background and form; footer color change and size

--- src/containers/ContactUs.js
+++ src/containers/ContactUs.js
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 class Banner extends Component {
   render() {
     return (
-      <div className="contactus-wrapper">
-        <div className="banner-gradient">
-          <div className="banner-text-container">
-            <p>"I didn’t know my Cosmetic Surgeon</p>
-            <p><span>wasn’t</span> a Plastic Surgeon."</p>
+      <div className="contactUs-wrapper">
+        <div className="contactUs-overlay">
+          <div className="contactUs-text-container">
+            <p>Contact Us</p>
+            <p>Please fill out your information</p>
+            <form action="">
+              <fieldset className="fieldset">
+                Full name:<br />
+                <input className="input-fields" type="text" name="fullname" />
+                <br />
+                Email:<br />
+                <input className="input-fields" type="text" name="email" />
+                <br />
+                Subject:<br />
+                <input className="input-fields" type="text" name="subject" />
+                <br />
+                Message:<br />
+                <textarea className="input-area" rows="12" />
+                <br /><br />
+                <input className="submit-btn" type="submit" value="Submit" />
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
