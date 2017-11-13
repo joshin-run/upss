6a4c388ed6a40fe2d9e88aa401af3dc400ff25ae
joshin-run <jahmsc@gmail.com>
2017-11-13 13:04:00 -0700

    Feat (style) info section, still more responsive at 481px and 769px, WIP

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -11,46 +11,48 @@ class Info extends Component {
   render() {
     return (
       <div className="info-wrapper">
-        <p className="info-section-title">
-          Our Mission
-        </p>
-        <div className="our-mission-container">
-          <img className="our-mission-image1" src={hexigonDoc} />
-          <img className="our-mission-image2" src={hexigonDocHalf} />
-          <div className="our-mission-content">
-            <img className="upss-logo-pink"  src={upssLogoPink} alt="UPSS" />
-            <div className="our-mission-text">
-              <p><span>The Utah Plastic Surgery Society’s mission is to:</span></p><br />
-              <p>Encourage the highest possible standards in the field of plastic surgery</p><br />
-              <p>Provide trustworthy information to prospective patients</p><br />
-              <p>Protect the public from irresponsible and unqualified practitioners</p>
+        <div className="page-container">
+          <p className="info-section-title">
+            Our Mission
+          </p>
+          <div className="our-mission-container">
+            <img className="our-mission-image1" src={hexigonDoc} />
+            <img className="our-mission-image2" src={hexigonDocHalf} />
+            <div className="our-mission-content">
+              <img className="upss-logo-pink"  src={upssLogoPink} alt="UPSS" />
+              <div className="our-mission-text">
+                <p><span>The Utah Plastic Surgery Society’s mission is to:</span></p><br />
+                <p>Encourage the highest possible standards in the field of plastic surgery</p><br />
+                <p>Provide trustworthy information to prospective patients</p><br />
+                <p>Protect the public from irresponsible and unqualified practitioners</p>
+              </div>
             </div>
           </div>
-        </div>
-        <p id="membership-info-section-title" className="info-section-title">
-          UPSS Membership
-        </p>
-        <div className="upss-membership-container">
-          <div className="upss-membership-content">
-            <div className="upss-membership-text">
-              <p>In order to qualify for membership in the Utah Plastic Surgery Society, a surgeon must be properly trained and be certified by the <a href="https://www.abplasticsurgery.org/" target="_blank"><span className="link">American Board of Plastic Surgery.</span></a></p><br />
-              <p>Prospective patients can be assured that UPSS surgeons have successfully met the most rigorous academic and professional requirements, and have demonstrated outstanding surgical skill throughout their careers.</p>
+          <p id="membership-info-section-title" className="info-section-title">
+            UPSS Membership
+          </p>
+          <div className="upss-membership-container">
+            <div className="upss-membership-content">
+              <div className="upss-membership-text">
+                <p>In order to qualify for membership in the Utah Plastic Surgery Society, a surgeon must be properly trained and be certified by the <a href="https://www.abplasticsurgery.org/" target="_blank"><span className="link">American Board of Plastic Surgery.</span></a></p><br />
+                <p>Prospective patients can be assured that UPSS surgeons have successfully met the most rigorous academic and professional requirements, and have demonstrated outstanding surgical skill throughout their careers.</p>
+              </div>
+              <img className="toon-doctor-image"  src={toonDoc} alt="Cartoon Male Doctor" />
             </div>
-            <img className="toon-doctor-image"  src={toonDoc} alt="Cartoon Male Doctor" />
           </div>
-        </div>
-        <p className="info-section-title">
-         American Society of<br />
-         Plastic Surgeons
-        </p>
-        <div className="asps-container">
-          <img className="asps-image1" src={steth} />
-          <img className="asps-image2" src={stethHalf} />
-          <div className="asps-content">
-            <img className="asps-logo" src={aspsLogo} alt="ASPS Logo" />
-            <div className="">
-              <p>Only a board certified plastic surgeons will provide the medical expertise you deserve.</p><br />
-              <p>Learn more about the <a href="https://www.plasticsurgery.org/" target="_blank"><span className="link">American Society of Plastic Surgeons</span></a></p><br />
+          <p className="info-section-title">
+           American Society of<br />
+           Plastic Surgeons
+          </p>
+          <div className="asps-container">
+            <img className="asps-image1" src={steth} />
+            <img className="asps-image2" src={stethHalf} />
+            <div className="asps-content">
+              <img className="asps-logo" src={aspsLogo} alt="ASPS Logo" />
+              <div className="">
+                <p>Only a board certified plastic surgeons will provide the medical expertise you deserve.</p><br />
+                <p>Learn more about the <a href="https://www.plasticsurgery.org/" target="_blank"><span className="link">American Society of Plastic Surgeons</span></a></p><br />
+              </div>
             </div>
           </div>
         </div>
