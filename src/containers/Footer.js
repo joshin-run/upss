b8fa05d22f2cae40beaa2351213d51ad963449b2
joshin-run <jahmsc@gmail.com>
2017-11-29 12:28:31 -0700

    Feat (style) footer, square logo

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import UPSSLogoSqWhite from '../images/logos/upss-logo-sq-wh.png'
 import facebook from '../images/icons/facebook-circ-wh2.png'
 import twitter from '../images/icons/twitter-circ-wh.png'
 import youtube from '../images/icons/youtube-circ-wh2.png'
@@ -7,26 +8,18 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer-wrapper">
-
         <div className="footer-info-container">
-          <div className="footer-info">
-            <p>Utah Plastic Surgery Society</p>
-            <p>310 East 4500 South</p>
-            <p>Suite 500</p>
-            <p>Salt Lake City, UT 84107-4250</p>
-            <p>(801) 747-3500</p>
-          </div>
+          <img className="upss-sq-logo" src={UPSSLogoSqWhite} alt="Square UPSS Logo" />
           <div className="footer-media">
             <img className="media" src={facebook} alt="Facebook" />
             <img className="media" src={twitter} alt="Twitter" />
             <img className="media" src={youtube} alt="Youtube" />
           </div>
-        </div>
-
-        <div className="footer-copy">
-          <div className="footer-copy-text">
-            <p className="copy-text">Copyright © 2017</p>
-            <p className="society-text">Utah Plastic Surgery Society. All Rights Reserved</p>
+          <div className="footer-copy">
+            <div className="footer-copy-text">
+              <p className="copy-text">Copyright © 2017</p>
+              <p className="society-text">Utah Plastic Surgery Society. All Rights Reserved</p>
+            </div>
           </div>
         </div>
 
